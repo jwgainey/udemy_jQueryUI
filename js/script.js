fe4ee04fc5accbe624b6b6c72d144ae976ecc0ed
jwgainey <jwgainey@hotmail.com>
2020-08-17 15:43:07 -0400

    j query ui - drag

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// script.js
+
+$(function() {
+
+    $(".box").draggable();
+
+    $("#box1").draggable({ scroll: true });
+    $("#box2").draggable({ axis: "x" });
+    $("#box3").draggable({ axis: "y" });
+    $("#box4").draggable({ containment: ".container" });
+
+
+
+
+    
+});
