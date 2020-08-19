e14faec75b0f97803a159ac9659f6c4b3f359819
jwgainey <jwgainey@hotmail.com>
2020-08-19 15:36:51 -0400

    droppable

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@
 
 $(function() {
 
+    // DRAGGABLE
     $(".box").draggable();
 
-    $("#box1").draggable({ scroll: true });
+    $("#box1").draggable({ scroll: true, revert: "invalid" });
     $("#box2").draggable({ axis: "x" });
     $("#box3").draggable({ axis: "y" });
     $("#box4").draggable({ containment: ".container" });
 
+    // DROPPABLE
+    $("#droppable").droppable({
+        accept: '#box1',
+        drop: function() {
+            $(this).find('span').html("when a box got attitude, drop it like its hot")
+        }
+    });
 
 
 
