aa36ac97b4f5a4d8443439cb4ab49ab259f815a5
jwgainey <jwgainey@hotmail.com>
2020-08-19 17:10:55 -0400

    to do list app

--- js/script.js
+++ js/script.js
@@ -37,4 +37,28 @@ $(function() {
         maxDate: "1W +5D"
     });
 
+    // TO DO LIST
+    $('#todoList ul').sortable ({
+        items:"li:not('.listTitle, .addItem')",
+        connectWith:"ul",
+        dropOnEmpty: true,
+        placeholder: "emptySpace"
+    });
+
+    $('input').keydown(function(e) {
+        if(e.keyCode == 13) {
+            var item = $(this).val();
+
+            $(this).parent().parent().append('<li>' + item + '</li>');
+            $(this).val('');
+        }
+    });
+
+    $('#trash').droppable({
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
+    })
+
+
 });
